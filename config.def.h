d6b2bb112db483870b747bf8f3f8ea98f1838453
legitnull <legitnull0@gmail.com>
2021-05-25 14:09:44 -0600

    added scroll

--- config.def.h
+++ config.def.h
@@ -95,9 +95,6 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
-
-/* bg opacity */
 float alpha = 0.8, alphaUnfocused = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
@@ -139,7 +136,7 @@ unsigned int defaultfg = 7;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 17, bgUnfocused = 16;
+unsigned int bg = 16, bgUnfocused = 16;
 
 /*
  * Default shape of cursor
